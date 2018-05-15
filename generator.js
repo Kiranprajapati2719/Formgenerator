1bf52a82825aaba21be6be0d1c2c357160085817
DESKTOP-42S1U5O\user <abishek.dulal666s@gmail.com>
2018-05-15 07:46:09 +0545

    search

--- generator.js
+++ generator.js
@@ -1,9 +1,13 @@
 "use strict"
-let arr=["input","textbox"];
+let arr=["input","textbox","checkbox"];
+let searcharr=[];
 let searchbox=document.getElementById('searchbox');
+let searchoption=document.getElementById('searchoptions');
 searchbox.addEventListener('input', function(e) {
   let inval = event.target.value;
-   arr.filter((item) => {
+  searcharr=[];
+  searchoption.innerHTML="";
+   arr.map((item)=>{ return item.toLowerCase()}).filter((item) => {
     if(inval.length>2){
      return item.includes(inval);
       }
@@ -11,6 +15,19 @@ searchbox.addEventListener('input', function(e) {
         return false;
       }
    }).forEach((item)=>{
-          alert(item);
+      if(!searcharr.includes(item)){
+        searchoption.insertAdjacentHTML("beforeend",`<p>${item}</p>`);
+        searcharr.push(item);
+      }
    });
+});
+
+searchbox.addEventListener("focusin",function(e){
+  searchoption.style.display="block";
+});
+
+searchbox.addEventListener("focusout",function(e){
+   searcharr=[];
+   searchoption.innerHTML="";
+  searchoption.style.display="none";
 });
